b514f558d2f7ffe7e4a01a69b405ac6abaab8745
Vijay <vachaudhari26@gmail.com>
2021-11-19 13:25:37 +0530

    ytube used

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
     # dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_handler(MessageHandler(Filters.text, getSong))
+    dp.add_handler(MessageHandler(Filters.text, sendSong))
 
     # log all errors
     dp.add_error_handler(error)
