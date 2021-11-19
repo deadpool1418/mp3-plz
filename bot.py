cd08afdccd9c511a237ac63ca4a9b911a6f76f2b
Vijay <vachaudhari26@gmail.com>
2021-11-19 12:17:27 +0530

    Code Modified

--- bot.py
+++ bot.py
@@ -71,7 +71,8 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, getSong))
 
     # log all errors
     dp.add_error_handler(error)
